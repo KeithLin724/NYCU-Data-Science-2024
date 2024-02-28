6df59b512a8437af903abe616352db660215e14f
KeithLin7024 <keithlin724@gmail.com>
2024-02-29 00:45:43 +0800

    update code is same line

--- HW1/Part1/109511276.py
+++ HW1/Part1/109511276.py
@@ -556,8 +556,7 @@ class CrawlerHW:
             table = table.drop(columns=["like_boo_type", "body"])
             dict_list = table.to_dict("records")
             # first 10
-            total = len(table)
-            top_10 = dict_list[: CrawlerHW.FIRST_TEN]
+            total, top_10 = len(table), dict_list[: CrawlerHW.FIRST_TEN]
 
             return {type_: {"total": total, "top10": top_10}}
 
