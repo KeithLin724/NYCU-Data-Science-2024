458ca8d4a4835d77ee614e0ba3a9db60d99e21bf
KeithLin7024 <keithlin724@gmail.com>
2024-03-06 16:23:30 +0800

    update some code

--- HW1/Part2/main.py
+++ HW1/Part2/main.py
@@ -1,13 +1,14 @@
 import json
 import os
-import time
 import sys
+import time
 
+import torchvision.transforms as transforms
 from torch import no_grad
 from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-from ModelHW import ImageDataset, ModelHW
 from tqdm import tqdm
+
+from ModelHW import ImageDataset, ModelHW
 
 
 class App:
@@ -28,15 +29,34 @@ class App:
             ]
         )
 
-    def run(self, file_name: str):
-        start_time = time.time()
-        assert os.path.exists(file_name)
+    @staticmethod
+    def read_file(file_name: str):
+        """
+        This Python function reads a JSON file and returns the "image_paths" data from it.
 
+        :param file_name: The `file_name` parameter is a string that represents the name or path of the file
+        that you want to read
+        :type file_name: str
+        :return: The function `read_file` is returning the value associated with the key "image_paths" from
+        the data read from the file specified by the `file_name` parameter.
+        """
         with open(file_name, mode="r", encoding="utf-8") as f:
             data_read = json.load(f)
 
-        images_list = data_read["image_paths"]
+        return data_read["image_paths"]
 
+    def prediction(self, images_list: list[str]) -> list[int]:
+        """
+        This Python function takes a list of image paths, processes them using a specified transform, makes
+        predictions using a model, and returns a dictionary containing the predictions as a list of
+        integers.
+
+        :param images_list: The `images_list` parameter is a list of file paths to images that you want to
+        make predictions on
+        :type images_list: list[str]
+        :return: A dictionary containing the key "image_predictions" with a list of integer predictions
+        based on the input images is being returned.
+        """
         dataset = ImageDataset(images_list, self.transform)
         data_loader = DataLoader(dataset, App.BATCH_SIZE)
 
@@ -54,6 +74,14 @@ class App:
         result = [int(item == App.HOT_TYPE) for item in result]
 
         result_dict = {"image_predictions": result}
+        return result_dict
+
+    def run(self, file_name: str):
+        start_time = time.time()
+        images_list = App.read_file(file_name)
+
+        result_dict = self.prediction(images_list)
+
         with open(file=App.OUTPUT_FILE_NAME, mode="w", encoding="utf-8") as f_output:
             json.dump(result_dict, f_output, indent=4)
 
