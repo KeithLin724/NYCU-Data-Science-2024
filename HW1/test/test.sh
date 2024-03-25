77108e60b1987b307983dd7c209d1248b9d8df12
KeithLin724 <keithlin724@gmail.com>
2024-03-25 13:28:36 +0800

    unlock the test

--- HW1/test/test.sh
+++ HW1/test/test.sh
@@ -1,20 +1,19 @@
 cd out
 
-# python ./109511276.py crawl
+python ./109511276.py crawl
 # push
 python ./109511276.py push 0101 0130
 python ./109511276.py push 0425 0426
 python ./109511276.py push 0930 1101
 
 # popular
-# python ./109511276.py popular 0213 0312
-# python ./109511276.py popular 0303 0329
-# python ./109511276.py popular 1002 1105
+python ./109511276.py popular 0213 0312
+python ./109511276.py popular 0303 0329
+python ./109511276.py popular 1002 1105
 
-
-
-# python ./109511276.py keyword 0701 0731 IG
-# python ./109511276.py keyword 0815 0910 PiTT
+# keyword
+python ./109511276.py keyword 0701 0731 IG
+python ./109511276.py keyword 0815 0910 PiTT
 
 cd ../
 pwd
