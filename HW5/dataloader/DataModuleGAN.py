e04900b0a23bd5ed135910501abe56d0e987db8a
KeithLin724 <keithlin724@gmail.com>
2024-05-20 13:27:48 +0800

    add data module gan

--- HW5/dataloader/DataModuleGAN.py
+++ HW5/dataloader/DataModuleGAN.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 
 import lightning as L
 
+# ref:
+## https://lightning.ai/docs/pytorch/stable/data/datamodule.html
+
 
 class DataModuleGAN(L.LightningDataModule):
     def __init__(self):
