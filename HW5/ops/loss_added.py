0442a1cc60c0f1f9d462977d86d359af9c29b9da
KeithLin724 <keithlin724@gmail.com>
2024-05-20 23:40:40 +0800

    make it cleaner

--- HW5/ops/loss_added.py
+++ HW5/ops/loss_added.py
@@ -22,23 +22,24 @@ class GANLoss(nn.Module):
 
     def get_target_tensor(self, input, target_is_real):
         target_tensor = None
+
+        create_f = lambda var_t: (var_t is None) or (var_t.numel() != input.numel())
+
         if target_is_real:
-            create_label = (self.real_label_var is None) or (
-                self.real_label_var.numel() != input.numel()
-            )
+            create_label = create_f(self.real_label_var)
+
             if create_label:
                 real_tensor = self.Tensor(input.size()).fill_(self.real_label)
                 self.real_label_var = Variable(real_tensor, requires_grad=False)
-            target_tensor = self.real_label_var
-        else:
-            create_label = (self.fake_label_var is None) or (
-                self.fake_label_var.numel() != input.numel()
-            )
-            if create_label:
-                fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
-                self.fake_label_var = Variable(fake_tensor, requires_grad=False)
-            target_tensor = self.fake_label_var
-        return target_tensor
+
+            return self.real_label_var
+        # else
+        create_label = create_f(self.fake_label_var)
+
+        if create_label:
+            fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
+            self.fake_label_var = Variable(fake_tensor, requires_grad=False)
+        return self.fake_label_var
 
     def __call__(self, input, target_is_real):
         target_tensor = self.get_target_tensor(input, target_is_real)
