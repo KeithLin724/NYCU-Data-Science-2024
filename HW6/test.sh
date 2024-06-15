5e040a7054cc7b5fc2d4d86b1e7cbe17503ef6ab
KeithLin724 <keithlin724@gmail.com>
2024-06-15 21:51:33 +0800

    update test.sh

--- HW6/test.sh
+++ HW6/test.sh
@@ -1,2 +1,2 @@
-python train.py --config configs/default.yaml --test --opts MODEL.CHECKPOINT "segmentation/best_model_epoch_100.pth"
-python create_submission.py --pred prediction --save-file pred.csv
+python train.py --config configs/default.yaml --save-path test_1 --test --opts MODEL.CHECKPOINT "segmentation/best_model_epoch_100.pth"
+python create_submission.py --pred test_1 --save-file pred.csv
